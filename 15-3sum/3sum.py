3beca88b9aab51a2bb1e28184082a02c97bc9e59
SRINADH <srinadh2826@gmail.com>
2026-09-25 18:37:55 +0530

    Time: 747 ms (63.71%) | Memory: 18.3 MB (81.91%) - LeetSync

---
+++ 15-3sum/3sum.py
@@ -0,0 +1,53 @@
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+
+        nums.sort()
+
+        n = len(nums)
+        res = []
+
+        for i in range(n - 2):
+
+            
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            j = i + 1
+            k = n - 1
+
+            while j < k:
+
+                total = nums[i] + nums[j] + nums[k]
+
+                if total == 0:
+                    res.append([nums[i], nums[j], nums[k]])
+
+                    j += 1
+                    k -= 1
+
+                    
+                    while j < k and nums[j] == nums[j - 1]:
+                        j += 1
+
+                    
+                    while j < k and nums[k] == nums[k + 1]:
+                        k -= 1
+
+                elif total < 0:
+                    j += 1
+
+                else:
+                    k -= 1
+
+        return res
+
+
+obj = Solution()
+
+threeSum = obj.threeSum([-1, 0, 1, 2, -1, -4])
+
+print(threeSum)
